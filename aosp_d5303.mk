9226745567bf538370a7d4eda4b4d647782761c4
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2014-11-12 09:51:06 +0100

    initial d5303 device support
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

---
+++ aosp_d5303.mk
@@ -0,0 +1,44 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TARGET_PREBUILT_KERNEL := kernel
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/$(TARGET_PREBUILT_KERNEL):kernel
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
+$(call inherit-product, device/sony/yukon/device.mk)
+$(call inherit-product, vendor/sony/tianchi/tianchi-vendor.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+PRODUCT_COPY_FILES += \
+    device/sony/tianchi/rootdir/system/usr/idc/cyttsp4_mt.idc:system/usr/idc/cyttsp4_mt.idc \
+    device/sony/tianchi/rootdir/system/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
+    device/sony/tianchi/rootdir/system/etc/sensor_def_somc.conf:system/etc/sensor_def_somc.conf \
+    device/sony/tianchi/rootdir/system/etc/sensors_calib.conf:system/etc/sensors_calib.conf \
+    device/sony/tianchi/rootdir/system/etc/sec_config:system/etc/sec_config \
+    device/sony/tianchi/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
+    device/sony/tianchi/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
+    device/sony/tianchi/rootdir/init.yukon.dev.rc:root/init.yukon.dev.rc \
+    device/sony/tianchi/rootdir/logo.rle:root/logo.rle
+
+PRODUCT_NAME := aosp_d5303
+PRODUCT_DEVICE := tianchi
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := Xperia T2 Ultra (AOSP)
+PRODUCT_LOCALES += xhdpi hdpi
+PRODUCT_MANUFACTURER := Sony
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=320
+
---
+++ rootdir/system/usr/idc/cyttsp4_mt.idc
@@ -0,0 +1,14 @@
+# Device Type
+touch.deviceType = touchScreen
+
+# Pressure
+touch.pressure.scale = 0.0039
+
+# Glove Mode
+touch.gloveMode.deflatePressure = 1
+pointercontroller.touch1.filename = /system/somc/touch/images/big_touch.png
+pointercontroller.touch1.hotSpotX = 180
+pointercontroller.touch1.hotSpotY = 180
+pointercontroller.touchsplash1.filename = /system/somc/touch/images/splash.png
+pointercontroller.touchsplash1.hotSpotX = 23
+pointercontroller.touchsplash1.hotSpotY = 23
