d1a1d415888877a3dd9e7db06402415b166d2db7
Adam Farden <adam@farden.cz>
2015-02-10 19:39:14 +0100

    Define the device's kernel defconfig
    for inline kernel building we have to define
    kernel config for each device

--- aosp_d5303.mk
+++ aosp_d5303.mk
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+TARGET_KERNEL_CONFIG := aosp_yukon_tianchi_defconfig
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
 
 # backlight brightness below 127 is useless
