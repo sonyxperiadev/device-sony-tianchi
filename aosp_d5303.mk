5a2a30765afdecdd1b3a168cdd46d05c3f4a79ae
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-04-28 14:57:09 +0200

    inherit-product cleanup
    make existing inherit-product mandatory and fail build
    if anything is missing
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_d5303.mk
+++ aosp_d5303.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 $(call inherit-product, device/sony/yukon/device.mk)
 $(call inherit-product, vendor/sony/tianchi/tianchi-vendor.mk)
-$(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
     device/sony/tianchi/overlay
