bbfbdaf28add64d9a4001027ad0081bede4f1764
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-07 20:54:24 +0100

    fix platform naming
    in initial commit platform was named Board and device which
    is confusing. This commit fixes the naming making things easy
    to understand

--- aosp_d5303.mk
+++ aosp_d5303.mk
@@ -19,7 +19,7 @@ TARGET_KERNEL_CONFIG := aosp_yukon_tianchi_defconfig
 BOARD_HAS_DIM_BACKLIGHT := true
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, device/sony/yukon/device.mk)
+$(call inherit-product, device/sony/yukon/platform.mk)
 $(call inherit-product, vendor/sony/tianchi/tianchi-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
