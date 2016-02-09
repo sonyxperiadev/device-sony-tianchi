c2ef2d5c3911da64e631abb93b45562380172ba1
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-09 20:01:36 +0100

    device rework
    We split device from aosp_device so that it can be included
    easy in custom projects.
    
    New projects should create a new target named <project>_device
    which contains project specific configuration and options.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_d5303.mk
+++ aosp_d5303.mk
@@ -14,58 +14,13 @@
 
 TARGET_KERNEL_CONFIG := aosp_yukon_tianchi_defconfig
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, device/sony/yukon/platform.mk)
-$(call inherit-product, vendor/sony/tianchi/tianchi-vendor.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/sony/tianchi/device.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-
-DEVICE_PACKAGE_OVERLAYS += \
-    device/sony/tianchi/overlay
-
-# Device etc
-PRODUCT_COPY_FILES += \
-    device/sony/tianchi/rootdir/system/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
-    device/sony/tianchi/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
-    device/sony/tianchi/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
-    device/sony/tianchi/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    device/sony/tianchi/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
-
-# IDC
-PRODUCT_COPY_FILES += \
-    device/sony/tianchi/rootdir/system/usr/idc/cyttsp4_mt.idc:system/usr/idc/cyttsp4_mt.idc
-
-PRODUCT_PACKAGES += \
-    keystore.msm8226
-
-# Device Init
-PRODUCT_PACKAGES += \
-    fstab.tianchi \
-    init.recovery.tianchi \
-    init.tianchi \
-    ueventd.tianchi
-
-# Lights
-PRODUCT_PACKAGES += \
-    lights.tianchi
-
-# Simple PowerHAL
-PRODUCT_PACKAGES += \
-    power.tianchi
-
-# NFC config
-PRODUCT_PACKAGES += \
-    nfc_nci.tianchi
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 PRODUCT_NAME := aosp_d5303
 PRODUCT_DEVICE := tianchi
 PRODUCT_MODEL := Xperia T2 Ultra (AOSP)
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
-
-PRODUCT_AAPT_CONFIG := large
-PRODUCT_AAPT_PREBUILT_DPI := hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=240 \
-    ro.usb.pid_suffix=1A9
